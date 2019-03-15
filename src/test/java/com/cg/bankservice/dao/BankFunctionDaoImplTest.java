bc6b73ecbb4dcafde1ffc1ac6dd21f22672fb300
karthik jaiswal <karthikraj1959@gmail.com>
2019-03-15 15:30:03 +0530

    After junit testing

--- src/test/java/com/cg/bankservice/dao/BankFunctionDaoImplTest.java
+++ src/test/java/com/cg/bankservice/dao/BankFunctionDaoImplTest.java
@@ -5,19 +5,41 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import com.cg.bankservice.dto.CustomerDetails;
+
 class BankFunctionDaoImplTest {
 
-	
-	
+	static BankFunctionDaoImpl backDao;
+	static CustomerDetails customer;
+
 	@BeforeAll
 	public static void init()
 	{
-		
+		backDao=new BankFunctionDaoImpl();
+		customer=new CustomerDetails();
 	}
 	
 	@Test
-	void test() {
+	void testRegisterUser() {
 		
+		customer.setFirstName("akshita");
+		customer.setLastName("Reddy");
+		customer.setEmailId("reddy@gmail");
+		customer.setPassword("12345");
+		customer.setAadharNo(548756124589l);
+		customer.setPancardNo("aksr45");
+		customer.setMobileNo(8745645568l);
+		customer.setAddress("Hyderabad");
+		customer.setBalance(0);
+		
+		assertEquals(1012, backDao.registerUser(customer));
+	}
+	
+	@Test
+	void testLoginUser()
+	{
+		
+		assertEquals(1001, backDao.loginUser(1001, "1245"));
 	}
 
 }
--- src/test/java/com/cg/bankservice/dao/TransactionDaoImplTest.java
+++ src/test/java/com/cg/bankservice/dao/TransactionDaoImplTest.java
@@ -1,14 +1,49 @@
 package com.cg.bankservice.dao;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import com.cg.bankservice.dto.TransactionDetails;
 
 class TransactionDaoImplTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	static TransactionDaoImpl transactionDao;
+	static TransactionDetails transacDetails;
+	@BeforeAll
+	public static void init()
+	{
+		transactionDao=new TransactionDaoImpl();
+		transacDetails=new TransactionDetails();
 	}
-
+	@Test
+	void testFundTransfer()
+	{
+		transacDetails.setTransactionId(105);
+		transacDetails.setFromAccountNo(1002l);
+		transacDetails.setToAccountNo(1009l);
+		transacDetails.setAmountTransfered(100l);
+		
+		assertEquals(transacDetails,transactionDao.fundTransfer(1002l,1009l,100l));
+	}
+	
+	@Test
+	void testDepositAmount()
+	{
+		
+		assertEquals(2000,transactionDao.depositAmount(1007l,0l));
+	}
+	@Test
+	void testWithdrawAmount()
+	{
+		
+		assertEquals(1000,transactionDao.withdrawAmount(1001l,100l));
+	}
+	@Test
+	void testShowBalance()
+	{
+		
+		assertEquals(2090,transactionDao.showBalance(1000l));
+	}
+	
 }
